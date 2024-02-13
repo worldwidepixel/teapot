fb9476795cc3b63d0421569ad39fe2063afd125a
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-02-12 20:42:32 -0800

    SEO 2

--- ateapot/error.vue
+++ ateapot/error.vue
@@ -91,3 +91,13 @@ export default {
     props: ['error'],
 };
 </script>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'I am a teapot. Are you?',
+  ogTitle: 'I am a teapot. Are you?',
+  description: 'The high-quality teapot experience.',
+  ogDescription: 'The high-quality teapot experience.',
+  ogImage: 'https://http.cat/418',
+})
+</script>

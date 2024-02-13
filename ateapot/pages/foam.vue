40e74e206db9dc4ab4fb7c8442b30c3b1c4fb6ea
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-02-12 21:08:51 -0800

    New teapot endpoints

---
+++ ateapot/pages/foam.vue
@@ -0,0 +1,25 @@
+<template>
+    <title> I am a teapot. </title>
+    <div class="a"></div>
+</template>
+
+<style>
+
+.a {
+    font-family: sans-serif;
+}
+
+</style>
+  
+<script>
+  export default {
+    async setup() {
+    if (true) {
+      throw createError({
+        statusCode: 206,
+        fatal: false,
+        message: "Partial Content",
+      })
+    }
+  }  }
+</script>

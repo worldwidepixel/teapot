22504877d04a304e51b28201ee890d6e802ab394
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-02-12 20:39:20 -0800

    SEO???

--- ateapot/app.vue
+++ ateapot/app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'I am a teapot. Are you?',
+  ogTitle: 'I am a teapot. Are you?',
+  description: 'The high-quality teapot experience.',
+  ogDescription: 'The high-quality teapot experience.',
+  ogImage: 'https://http.cat/418',
+})
+</script>

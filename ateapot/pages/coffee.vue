76108738721baf8a269eb1aad0ca4ed0fef00d39
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-02-12 20:54:26 -0800

    Create coffee.vue

---
+++ ateapot/pages/coffee.vue
@@ -0,0 +1,25 @@
+<template>
+    <title> I am a teapot. </title>
+    <div class="a"></div>
+</template>
+
+<style>
+
+.a {
+    font-family: sans-serif;
+}
+
+</style>
+  
+<script>
+  export default {
+    async setup() {
+    if (true) {
+      throw createError({
+        statusCode: 503,
+        fatal: false,
+        message: "As a teapot, I cannot fulfill that request.",
+      })
+    }
+  }  }
+</script>
